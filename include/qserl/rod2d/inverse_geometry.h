12c3e31201d4277393a0397367984df1a3cad0c2
Olivier Roussel <olivier.roussel@laas.fr>
2015-02-25 18:13:52 +0100

    added inverse geometry prototype (based on Newton's method) for 2d rods.

---
+++ include/qserl/rod2d/inverse_geometry.h
@@ -0,0 +1,59 @@
+/**
+* Copyright (c) 2012-2014 CNRS
+* Author: Olivier Roussel
+*
+* This file is part of the qserl package.
+* qserl is free software: you can redistribute it
+* and/or modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation, either version
+* 3 of the License, or (at your option) any later version.
+*
+* qserl is distributed in the hope that it will be
+* useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Lesser Public License for more details.  You should have
+* received a copy of the GNU Lesser General Public License along with
+* qserl.  If not, see
+* <http://www.gnu.org/licenses/>.
+**/
+
+/** Helper for analytic computation of mu values. 
+* Just for experimentation, should not be used as long term.
+*/
+
+
+#ifndef QSERL_ROD2_INVERSE_GEOMETRY_Q_H_
+#define QSERL_ROD2_INVERSE_GEOMETRY_Q_H_
+
+#include "qserl/exports.h"
+
+#pragma warning( push, 0 )	
+#include <Eigen/Lgsm>
+#pragma warning( pop )	
+
+namespace qserl {
+namespace rod2d {
+
+/**
+* \param[int] i_q_des Rod geometry q(1) (i.e. at position t=1) where:
+*             i_q_des[0] = theta
+*             i_q_des[1] = x
+*             i_q_des[2] = y
+* \param[int] i_a0 Initial guess of rod parameterization in A-space where:
+*             i_a0[0] is rod base torque
+*             i_a0[1] is rod base force along x
+*             i_a0[2] is rod base force along y
+* \param[in]  i_alpha scaling factor for the descent step.
+* \param[out] o_a Resulting rod parameterization in A-space where:
+*             a[0] is rod base torque
+*             a[1] is rod base force along x
+*             a[2] is rod base force along y
+* \return true if successfull, false if unstability or singularity found.
+*/
+QSERL_EXPORT bool inverseGeometry_Newton(const Eigen::Vector3d& i_q_des, int i_maxIter, double i_maxNormError, 
+  const Eigen::Vector3d& i_a0, double i_alpha, Eigen::Vector3d& o_a);
+
+}	// namespace rod2d
+}	// namespace qserl
+
+#endif // QSERL_ROD2_INVERSE_GEOMETRY_Q_H_
---
+++ src/rod2d/inverse_geometry.cc
@@ -0,0 +1,98 @@
+/**
+* Copyright (c) 2012-2014 CNRS
+* Author: Olivier Roussel
+*
+* This file is part of the qserl package.
+* qserl is free software: you can redistribute it
+* and/or modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation, either version
+* 3 of the License, or (at your option) any later version.
+*
+* qserl is distributed in the hope that it will be
+* useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Lesser Public License for more details.  You should have
+* received a copy of the GNU Lesser General Public License along with
+* qserl.  If not, see
+* <http://www.gnu.org/licenses/>.
+**/
+
+/** Helper for computation of mu values. 
+* Just for experimentation, should not be used as long term.
+*/
+
+#include "qserl/rod2d/inverse_geometry.h"
+
+#include "qserl/rod2d/analytic_dqda.h"
+#include "util/timer.h"
+#include "util/utils.h"
+
+namespace qserl {
+namespace rod2d {
+
+bool inverseGeometry_Newton(const Eigen::Vector3d& i_q_des, int i_maxIter, double i_maxNormError, 
+  const Eigen::Vector3d& i_a0, double i_alpha, Eigen::Vector3d& o_a)
+{
+  static const double t = 1.;
+  static const double kJacobianDetNullTolerance = 1.e-9;
+
+	util::TimePoint startSolveTime = util::getTimePoint();
+
+  const double sqrdMaxNormError = util::sqr(i_maxNormError);
+  bool isQ1Close = false;
+  bool isUnstable = false;
+  bool singularityFound = false;
+  MotionConstantsDqDa mc_a_k;
+  Eigen::Matrix3d dq_da_k;  // jacobian dq(t) / da for a_k
+  Eigen::Matrix3d inv_dq_da_k;  // inverse of jacobian dq(t) / da for a_k
+  Eigen::Vector3d q_a_k, q_dot_a_k;  // q(t) for a_k and q_dot(t) for a_k (unused)
+  Eigen::Vector3d a_k = i_a0;
+  
+  int iter = 0;
+  while (!isQ1Close && iter < i_maxIter && !singularityFound && !isUnstable)
+  {
+    // compute common motion constants to dq / da and q for a_k 
+    singularityFound = !computeMotionConstantsDqDa(a_k, mc_a_k) ||
+      !computeQAtPositionT(t, a_k, mc_a_k.qc, q_dot_a_k, q_a_k) ||
+      !computeDqDaAtPositionT(t, mc_a_k, dq_da_k);
+    if (!singularityFound)
+    {
+      const Eigen::Vector3d r_a_k = q_a_k - i_q_des;
+      std::cout << "[DEBUG] [iter=" << iter << "] a_k = " << a_k << std::endl;
+      std::cout << "[DEBUG] [iter=" << iter << "] q(a_k) = " << q_a_k << std::endl;
+      std::cout << "[DEBUG] [iter=" << iter << "] ||r(a_k)|| = " << r_a_k.norm() << std::endl;
+      if (r_a_k.squaredNorm() > sqrdMaxNormError)
+      {
+        // inverse the jacobian and check rank
+        bool isJacobianInvertible;
+        double det_J_k;
+        dq_da_k.computeInverseAndDetWithCheck(inv_dq_da_k, det_J_k, isJacobianInvertible, kJacobianDetNullTolerance);
+        if (isJacobianInvertible)
+        {
+          const Eigen::Vector3d p_k = -inv_dq_da_k * r_a_k * i_alpha;
+          a_k = a_k + p_k;
+        }else{
+          std::cout << "[WARNING] inverseGeometry_Newton(): unstability point found at iter = " << iter << std::endl;
+          isUnstable = true;
+        }
+      }else{
+        // solution found
+        isQ1Close = true;
+      }
+    }else{
+      std::cout << "[WARNING] inverseGeometry_Newton(): singularity point found at iter = " << iter << std::endl;
+    }
+    ++iter;
+  }
+	double solveTimeUs = static_cast<double>(util::getElapsedTimeUsec(startSolveTime).count());
+
+  const bool success = !singularityFound && !isUnstable;
+  if (success)
+    std::cout << "[PROGRESS] inverseGeometry_Newton(): succefully solved after " << iter << " iterations" <<
+    " _ took " << solveTimeUs << "us" << std::endl;
+  return success;
+}
+
+
+}	// namespace rod2d
+}	// namespace qserl
